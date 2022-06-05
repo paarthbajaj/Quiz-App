34b4e2559c08377ebd625c230816395f927ae4d7
paarthbajaj <paarthbajaj14y@gmail.com>
2022-06-05 22:06:57 +0530

    refactor: changed favicon and default user details

--- src/backend/db/users.js
+++ src/backend/db/users.js
@@ -8,10 +8,10 @@ import { formatDate } from "../utils/authUtils";
 export const users = [
   {
     _id: uuid(),
-    firstName: "Adarsh",
-    lastName: "Balika",
-    email: "adarshbalika@gmail.com",
-    password: "adarshBalika123",
+    firstName: "admin",
+    lastName: "user",
+    email: "adminuser@gmail.com",
+    password: "adminUser123",
     createdAt: formatDate(),
     updatedAt: formatDate(),
   },
